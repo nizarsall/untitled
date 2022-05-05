693f91fb0940b3e75e0a780e8484d4429f79b9bd
Yami13K <alkayyalyamen@gmail.com>
2022-05-05 17:02:53 +0300

    background

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,49 @@ import 'package:google_map_polyline/google_map_polyline.dart';
 import 'database/funcs.dart';
 import 'package:intl/intl.dart';
 
+const EVENTS_KEY = "fetch_events";
+
 
 final f = funcs.instance;
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  var taskId = task.taskId;
+  var timeout = task.timeout;
+  if (timeout) {
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }
+
+  print("[BackgroundFetch] Headless event received: $taskId");
+
+  var timestamp = DateTime.now();
+
+  var prefs = await SharedPreferences.getInstance();
+
+  // Read fetch_events from SharedPreferences
+  var events = <String>[];
+  var json = prefs.getString(EVENTS_KEY);
+  if (json != null) {
+    events = jsonDecode(json).cast<String>();
+  }
+  // Add new event.
+  events.insert(0, "$taskId@$timestamp [Headless]");
+  // Persist fetch events in SharedPreferences
+  prefs.setString(EVENTS_KEY, jsonEncode(events));
+
+  if (taskId == 'flutter_background_fetch') {
+    main();
+    BackgroundFetch.scheduleTask(TaskConfig(
+        taskId: "yami",
+        delay: 5000,
+        periodic: false,
+        forceAlarmManager: false,
+        stopOnTerminate: false,
+        enableHeadless: true
+    ));
+  }
+  BackgroundFetch.finish(taskId);
+}
 
 void main() {
   runApp(MyApp());
@@ -72,10 +113,9 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-
+    initPlatformState();
 
     f.query();
-
     LocationManager().interval = 1;
     LocationManager().distanceFilter = 0;
     LocationManager().notificationTitle = 'CARP Location Example';
@@ -85,13 +125,99 @@ class _MyAppState extends State<MyApp> {
     _status = LocationStatus.INITIALIZED;
   }
 
+  Future<void> initPlatformState() async {
+    // Load persisted fetch events from SharedPreferences
+    var prefs = await SharedPreferences.getInstance();
+    var json = prefs.getString(EVENTS_KEY);
+    if (json != null) {
+      setState(() {
+        _events = jsonDecode(json).cast<String>();
+      });
+    }
+
+    // Configure BackgroundFetch.
+    try {
+      var status = await BackgroundFetch.configure(BackgroundFetchConfig(
+          minimumFetchInterval: 5,
+          forceAlarmManager: false,
+          stopOnTerminate: false,
+          startOnBoot: true,
+          enableHeadless: true,
+          requiresBatteryNotLow: false,
+          requiresCharging: false,
+          requiresStorageNotLow: false,
+          requiresDeviceIdle: false,
+          requiredNetworkType: NetworkType.NONE
+      ), _onBackgroundFetch, _onBackgroundFetchTimeout);
+      print('[BackgroundFetch] configure success: $status');
+
+      // Schedule a "one-shot" custom-task in 10000ms.
+      // These are fairly reliable on Android (particularly with forceAlarmManager) but not iOS,
+      // where device must be powered (and delay will be throttled by the OS).
+      BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: "yami",
+          delay: 100,
+          periodic: false,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true
+      ));
+    } on Exception catch(e) {
+      print("[BackgroundFetch] configure ERROR: $e");
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+
+  void _onBackgroundFetch(String taskId) async {
+    var prefs = await SharedPreferences.getInstance();
+    var timestamp = DateTime.now();
+    // This is the fetch-event callback.
+    print("[BackgroundFetch] Event received: $taskId");
+    setState(() {
+      _events.insert(0, "$taskId@${timestamp.toString()}");
+    });
+    // Persist fetch events in SharedPreferences
+    prefs.setString(EVENTS_KEY, jsonEncode(_events));
+
+    if (taskId == "yami") {
+      start();
+      main();
+      // Schedule a one-shot task when fetch event received (for testing).
+      /*
+      BackgroundFetch.scheduleTask(TaskConfig(
+          taskId: "yami",
+          delay: 5000,
+          periodic: false,
+          forceAlarmManager: true,
+          stopOnTerminate: false,
+          enableHeadless: true,
+          requiresNetworkConnectivity: true,
+          requiresCharging: true
+      ));
+       */
+    }
+    // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+    // for taking too long in the background.
+    BackgroundFetch.finish(taskId);
+  }
+
+  /// This event fires shortly before your task is about to timeout.  You must finish any outstanding work and call BackgroundFetch.finish(taskId).
+  void _onBackgroundFetchTimeout(String taskId) {
+    print("[BackgroundFetch] TIMEOUT: $taskId");
+    BackgroundFetch.finish(taskId);
+  }
+
+
 
 
   void getCurrentLocation() async =>
       onData(await LocationManager().getCurrentLocation());
 
   LocationDto onData(LocationDto dto) {
-    print(dtoToString(dto));
     LatLng p = new LatLng(dto.latitude, dto.longitude);
     if (p != null) {
       points.add(p);
@@ -99,7 +225,6 @@ class _MyAppState extends State<MyApp> {
 
     i++;
     print("inside");
-    print(dto);
     f.insert(dto.latitude, dto.longitude,'${DateTime.fromMillisecondsSinceEpoch(dto.time ~/ 1)}');
     if (i == 1) {
       _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
